0e7bf8e9fb856d493ed7f87d8490bde9cfc8819a
CodedMasonry <109301662+CodedMasonry@users.noreply.github.com>
2024-12-08 19:45:24 -0500

    Add padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,15 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <main className="mt-64 flex flex-col items-center justify-center">
-      <h1 className="text-6xl font-bold underline decoration-primary">
+    <main className="mt-64 flex flex-col items-center justify-center mx-4">
+      <h1 className="text-3xl md:text-6xl font-bold underline decoration-primary">
         Hey, you spelled my name wrong
       </h1>
-      <h2 className="mt-8 text-4xl font-semibold">Schaffer -&gt; Shaffer</h2>
+      <h2 className="mt-8 text-2xl md:text-4xl font-semibold">Schaffer -&gt; Shaffer</h2>
 
       <Link
         href="https://brockshaffer.dev"
-        className="mt-10 rounded-xl border-4 border-primary bg-primary p-4 text-4xl transition hover:border-primary-foreground/90 hover:bg-primary/90"
+        className="mt-10 rounded-xl border-4 border-primary bg-primary p-4 text-xl md:text-4xl transition hover:border-primary-foreground/90 hover:bg-primary/90"
       >
         Click Here For A Redirect
       </Link>
