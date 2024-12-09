4ecf54051fafd1b620e0ed9c9084adcc21863e22
Mason <109301662+CodedMasonry@users.noreply.github.com>
2024-12-09 08:12:48 -0500

    Fix mobile wrapping

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,18 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <main className="mt-64 flex flex-col items-center justify-center mx-4">
-      <h1 className="text-3xl md:text-6xl font-bold underline decoration-primary">
-        Hey, you spelled my name wrong
+    <main className="px-4 mx-auto mt-28 flex flex-col items-center justify-center text-center md:mt-64">
+      <h1 className="text-3xl font-bold underline decoration-primary md:text-6xl">
+        Hey, you spelt my name wrong
       </h1>
-      <h2 className="mt-8 text-2xl md:text-4xl font-semibold">Schaffer -&gt; Shaffer</h2>
+      <h2 className="mt-8 text-2xl font-semibold md:text-4xl">
+        Schaffer -&gt; Shaffer
+      </h2>
 
       <Link
         href="https://brockshaffer.dev"
-        className="mt-10 rounded-xl border-4 border-primary bg-primary p-4 text-xl md:text-4xl transition hover:border-primary-foreground/90 hover:bg-primary/90"
+        prefetch={true}
+        className="mt-10 rounded-xl border-4 border-primary bg-primary p-4 text-xl transition hover:border-primary-foreground/90 hover:bg-primary/90 md:text-4xl"
       >
         Click Here For A Redirect
       </Link>
